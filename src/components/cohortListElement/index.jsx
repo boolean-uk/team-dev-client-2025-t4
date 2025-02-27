68884037a394fc15b20422126f5852aba6ab2145
heskinstad <heskinstad@gmail.com>
2025-02-27 08:35:17 +0100

    Fixed css naming

--- src/components/cohortListElement/index.jsx
+++ src/components/cohortListElement/index.jsx
@@ -10,7 +10,7 @@ function CohortListElement() {
         <div className="cohortListElement">
             <div className="cohortIcon"><SquareBracketsIcon></SquareBracketsIcon></div> {/*TODO: add colored circle around icon (and correct icon)*/}
             <div className="cohortListElementTitle"><p>Software Development</p></div>
-            <div className="cohortListElementTimeframe"><p>Cohort 4</p></div>
+            <div className="cohortListElementNumber"><p>Cohort 4</p></div>
         </div>
     )
 }
